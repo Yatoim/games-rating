4abdcf162eeae09f9da068e0543de71192431856
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-02 23:57:42 +0300

    Add files via upload

---
+++ proect/appModules/api/index.js
@@ -0,0 +1,8 @@
+const {getData, getRandomGame} = require("./api-untils")
+const endpoints = require("./config")
+
+module.exports = {
+    endpoints,
+    getData,
+    getRandomGame,
+};
---
+++ proect/controllers/index.js
@@ -0,0 +1,10 @@
+const gameRouteController = require ("./game")
+const defaultRouteController = require ("./default")
+const voteRouteController = require ("./vote")
+const mainRouteController = require ("./main")
+module.exports = {
+ mainRouteController,
+ gameRouteController,
+ defaultRouteController,
+ voteRouteController,
+}
